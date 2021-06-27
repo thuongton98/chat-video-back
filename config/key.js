2262b81dea6131d5e42036957de7d10f5ef5fe8f
thuong <thuonglovevietnam@gmail.com>
2021-06-27 14:24:53 +0700

    t

--- config/key.js
+++ config/key.js
@@ -1,6 +1,6 @@
-dbPassword = 'mongodb://localhost:27017/chat';
+//dbPassword = 'mongodb://localhost:27017/chat';
 
-//dbPassword = 'mongodb+srv://thuongadmin:thuong123@cluster0.gt3dp.mongodb.net/chat?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://thuongadmin:thuong123@cluster0.gt3dp.mongodb.net/chat?retryWrites=true&w=majority';
 
 
 module.exports = {
